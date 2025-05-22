f8fa45125473db1f767f8df7764adb4a90ce1f50
Richard Essuman <ressuman001@gmail.com>
2025-05-22 09:43:42 +0000

    `Remove export keyword from authOptions and handler in route.ts`
    refactor: Change export to const for authOptions and handler in NextAuth setup

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -26,7 +26,7 @@ declare module "next-auth" {
   }
 }
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       id: "credentials",
@@ -196,6 +196,6 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
